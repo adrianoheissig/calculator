ee8c3138a8166d0343dce586e7484f04441b99c8
Adriano Heissig <91757217+adrianorosset@users.noreply.github.com>
2023-02-13 08:55:35 -0300

    Melhorado componente Button

--- src/App.js
+++ src/App.js
@@ -8,27 +8,41 @@ export default class App extends Component {
   state = {
     displayValue: '0',
   };
+
+  clearMemory = () => {
+    this.setState({displayValue: 0});
+  };
+
+  addDigit = n => {
+    this.setState({displayValue: n});
+  };
+
+  setOperation = operation => {
+    console.warn(operation);
+  };
+
   render() {
     return (
       <View style={style.container}>
         <Display value={this.state.displayValue} />
         <View style={style.buttons}>
-          <Button label="AC" />
-          <Button label="/" />
-          <Button label="7" />
-          <Button label="8" />
-          <Button label="9" />
-          <Button label="*" />
-          <Button label="4" />
-          <Button label="5" />
-          <Button label="6" />
-          <Button label="-" />
-          <Button label="1" />
-          <Button label="2" />
-          <Button label="3" />
-          <Button label="+" />
-          <Button label="0" />
-          <Button label="." />
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={this.setOperation} />
+          <Button label="7" onClick={this.addDigit} />
+          <Button label="8" onClick={this.addDigit} />
+          <Button label="9" onClick={this.addDigit} />
+          <Button label="*" operation onClick={this.setOperation} />
+          <Button label="4" onClick={this.addDigit} />
+          <Button label="5" onClick={this.addDigit} />
+          <Button label="6" onClick={this.addDigit} />
+          <Button label="-" operation onClick={this.setOperation} />
+          <Button label="1" onClick={this.addDigit} />
+          <Button label="2" onClick={this.addDigit} />
+          <Button label="3" onClick={this.addDigit} />
+          <Button label="+" operation onClick={this.setOperation} />
+          <Button label="0" double onClick={this.addDigit} />
+          <Button label="." onClick={this.addDigit} />
+          <Button label="=" operation onClick={this.setOperation} />
         </View>
       </View>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,19 @@ import React from 'react';
 import {Text, StyleSheet, TouchableHighlight, Dimensions} from 'react-native';
 
 export default props => {
+  const stylesButton = [style.button];
+  if (props.operation) {
+    stylesButton.push(style.buttonOperation);
+  }
+  if (props.double) {
+    stylesButton.push(style.buttonDouble);
+  }
+  if (props.triple) {
+    stylesButton.push(style.buttonTriple);
+  }
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={style.button}>{props.label}</Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
 };
@@ -20,5 +30,15 @@ const style = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 1,
     borderColor: '#888',
+  },
+  buttonOperation: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
   },
 });
